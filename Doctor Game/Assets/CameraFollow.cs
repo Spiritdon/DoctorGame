06e7ce66b760817754640ea01b8d650bfc3f76cf
niantoniadis <naa3207@rit.edu>
2021-03-12 14:18:28 -0500

    cam 2.0

--- Doctor Game/Assets/CameraFollow.cs
+++ Doctor Game/Assets/CameraFollow.cs
@@ -19,6 +19,30 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), transform.position.y, Mathf.Clamp(target.position.z, minZ, maxZ));
+        bool controlled = false;
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z-5*Time.deltaTime, minZ, maxZ));
+            controlled = true;
+        }
+        else if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z + 5 * Time.deltaTime, minZ, maxZ));
+            controlled = true;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x - 5 * Time.deltaTime, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z, minZ, maxZ));
+            controlled = true;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x + 5 * Time.deltaTime, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z, minZ, maxZ));
+            controlled = true;
+        }
+        if(!controlled)
+        {
+            transform.position = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), transform.position.y, Mathf.Clamp(target.position.z, minZ, maxZ));
+        }
     }
 }
