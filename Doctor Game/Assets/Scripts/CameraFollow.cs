4c29331a1156d464ed33a01010ba55377fc76c88
niantoniadis <naa3207@rit.edu>
2021-03-19 14:18:11 -0400

    cam collider bounds done

--- Doctor Game/Assets/Scripts/CameraFollow.cs
+++ Doctor Game/Assets/Scripts/CameraFollow.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float minX;
-    public float maxX;
-    public float minZ;
-    public float maxZ;
+    Collider limits;
+    float zExtent;
+    float xExtent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        limits = transform.parent.GetComponent<Collider>();
+        zExtent = Camera.main.orthographicSize;
+        xExtent = zExtent * Camera.main.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
         bool controlled = false;
+        float minX = limits.bounds.center.x-limits.bounds.extents.x + xExtent;
+        float maxX = limits.bounds.center.x + limits.bounds.extents.x - xExtent;
+        float minZ = limits.bounds.center.z - limits.bounds.extents.z + zExtent;
+        float maxZ = limits.bounds.center.z + limits.bounds.extents.z - zExtent;
+
         if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z-5*Time.deltaTime, minZ, maxZ));
