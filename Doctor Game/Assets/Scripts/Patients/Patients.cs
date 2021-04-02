b26dd2049332869a722eb3ad90f7e5fd6c7320e9
John Zhang <1724118498@qq.com>
2021-04-03 02:27:08 +0800

    push

--- Doctor Game/Assets/Scripts/Patients/Patients.cs
+++ Doctor Game/Assets/Scripts/Patients/Patients.cs
@@ -4,38 +4,48 @@ using UnityEngine;
 
 public class Patients : MonoBehaviour
 {
-    public GameObject patientGameObject;
+    public GameObject patientWithBed;
+    public GameObject patientObj;
+    public GameObject bedObj;
     private GameObject healthBar;
     public int health;
     private int cDay;
 
     void Awake()
     {
-        if (patientGameObject.GetComponent<Patients>().healthBar != null)
+        if (patientWithBed.GetComponent<Patients>().healthBar != null)
         {
             Debug.Log("1");
-            DontDestroyOnLoad(patientGameObject);
+            DontDestroyOnLoad(patientWithBed);
         }
         else
         {
             cDay = 1;
             healthBar = Resources.Load("HealthBarCanvas") as GameObject;
-            healthBar = Instantiate(healthBar, new Vector3(patientGameObject.transform.position.x, patientGameObject.transform.position.y + 10, patientGameObject.transform.position.z + 1.5f), Quaternion.identity);
+            healthBar = Instantiate(healthBar, new Vector3(patientWithBed.transform.position.x, patientWithBed.transform.position.y + 10, patientWithBed.transform.position.z + 1.5f), Quaternion.identity);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Stats.Day != cDay)
+        if (patientObj != null)
         {
-            cDay++;
-            health--;
-
-            if (health == 0)
+            if (Stats.Day != cDay)
             {
-                Destroy(this.gameObject);
+                cDay++;
+                health--;
+
+                if (health == 0)
+                {
+                    Destroy(patientObj);
+                    bedObj.GetComponent<Renderer>().material.color = Color.red;
+                }
+                else if (health >= 7)
+                {
+                    Destroy(patientObj);
+                    bedObj.GetComponent<Renderer>().material.color = Color.blue;
+                }
             }
         }
 
